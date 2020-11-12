a1ad889693761a6558b05cdd6b0a492e26123158
Gusarov Artem <artemiy.gusar@gmail.com>
2020-11-12 16:53:21 +0500

    Add words count

--- viceversa/views.py
+++ viceversa/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, 'home.html', {'wellcome':'Good time to say wellcome'})
+
+def reverse(request):
+	text = request.GET['usertext']
+	txt = text[::-1]
+	len_value = len(text.split())
+	return render(request, 'reverse.html', {'usertext':text, 'reversedtext':txt, 'lentext':len_value})
